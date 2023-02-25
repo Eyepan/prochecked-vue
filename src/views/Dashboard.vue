c15adcd069e04777a7b62f3a25c7f4b72d36c0dd
Pan <68371938+Eyepan@users.noreply.github.com>
2023-02-25 08:56:51 +0530

    started on dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-import { useUserStore } from "@/stores/userDetails";
 import axios from "axios";
-import { storeToRefs } from "pinia";
 import { onMounted } from "vue";
+import { useUserStore } from "@/stores/userDetails";
+import { storeToRefs } from "pinia";
 const { currentUser, projects, isLoggedIn } = storeToRefs(useUserStore());
 
 const router = useRouter();
@@ -31,17 +31,49 @@ function signOut() {
   isLoggedIn.value = false;
   localStorage.removeItem("currentUser");
   router.replace("/");
+  router.go(0);
 }
 </script>
 
 <template>
-  <section>
-    <button class="float-right" @click="signOut">Sign-out</button>
-    Hi {{ currentUser.email }}
+  <div
+    id="sidebar"
+    class="absolute md:w-1/4 lg:w-1/5 md:p-4 md:h-screen bg-white dark:bg-black flex justify-center"
+  >
+    <div class="mt-10">
+      <p class="text-xl hover:font-semibold text-gray-800 dark:text-white">
+        {{ currentUser.email }}
+      </p>
+      <p class="text-xl hover:font-semibold text-gray-800 dark:text-white">
+        {{ projects.length }} Projects
+      </p>
 
-    <div v-if="projects.length > 0" v-for="project in projects">
-      {{ project.title }}
+      <div class="mt-10">
+        <p class="text-xl hover:font-semibold text-gray-800 dark:text-white">
+          Projects
+        </p>
+        <div class="mt-5">
+          <div
+            v-for="project in projects"
+            :key="project.id"
+            class="flex justify-between"
+          >
+            <p
+              class="text-xl hover:font-semibold text-gray-800 dark:text-white"
+            >
+              {{ project.title }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div v-else>You haven't created a project yet, create one now!</div>
-  </section>
+
+    <button
+      class="absolute left-1/2 -translate-x-1/2 p-2 rounded-xl bottom-10 hover:bg-[var(--color-3)]"
+      @click="signOut"
+    >
+      Sign-out
+    </button>
+  </div>
+  <section id="content" class="absolute md:left-1/4 md:h-screen"></section>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,27 +3,34 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <section>
+  <section class="lg:m-20">
     <img class="float-right" src="@/assets/images/home.png" alt="" />
     <div class="text-5xl md:text-7xl font-extrabold">Pro-Checked</div>
     <div class="text-l md:text-xl font-bold">
       The easy way to organize a project
     </div>
 
-    <div class="text-l md:text-xl m-10 ml-0">
-      Design your next project with the confidence of Pro-Checked. An open
-      source Project Management tool that makes management simple and powerful
+    <div class="text-l md:text-xl m-10 mb-0 ml-0">
+      Design your next project with the confidence of Pro-Checked.
+    </div>
+    <div class="text-m md:text-l m-4 ml-0">
+      An open source tool that makes managing a project hassle-free
     </div>
     <div class="absolute top-2/3">
       <RouterLink to="/signup" class="mt-5 btn-primary">
         Get started!
       </RouterLink>
-      <button
+      <a
+        href="https://github.com/eyepan/prochecked-vue"
+        target="_blank"
         class="mt-20 m-2 p-4 rounded-xl bg-white dark:bg-black hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black"
       >
         <i class="fa fa-github p-1"></i>
         View on GitHub
-      </button>
+      </a>
+      <RouterLink to="/signin" class="mt-5 btn-primary bg-[var(--color-2)]">
+        Sign-in
+      </RouterLink>
     </div>
   </section>
 </template>
