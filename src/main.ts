d61acf287f7215cee15baf5f95973b5ef7bde1f8
Pan <68371938+Eyepan@users.noreply.github.com>
2023-03-09 18:50:34 +0530

    changed pwa, ready for flutter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
-
 import App from "./App.vue";
 import router from "./router/router";
 
