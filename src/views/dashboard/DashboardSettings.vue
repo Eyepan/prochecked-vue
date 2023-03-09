3287ae0ef5e37ff006f324d578c2821299babed3
Pan <68371938+Eyepan@users.noreply.github.com>
2023-03-09 13:35:48 +0530

    showaffirmations

--- src/views/dashboard/DashboardSettings.vue
+++ src/views/dashboard/DashboardSettings.vue
@@ -9,15 +9,15 @@ const { showGraphs, showAffirmations } = storeToRefs(useSettingsStore());
     <div class="text-7xl font-black mb-5">Settings</div>
     <div class="text-2xl font-bold">Edit your settings</div>
     <div class="flex mt-5 items-center justify-between">
-      <label for="showgraphs">Show graphs</label>
-      <select name="" id="" v-model="showGraphs">
+      <label for="showGraphs">Show graphs</label>
+      <select name="" id="showGraphs" v-model="showGraphs">
         <option value="true">Show</option>
         <option value="false">Hide</option>
       </select>
     </div>
     <div class="flex mt-5 items-center justify-between">
-      <label for="showaffirmations">Show Affirmations</label>
-      <select name="" id="" v-model="showAffirmations">
+      <label for="showAffirmations">Show Affirmations</label>
+      <select name="" id="showAffirmations" v-model="showAffirmations">
         <option value="true">Show</option>
         <option value="false">Hide</option>
       </select>
